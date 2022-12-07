4f1a4d2e0509e768aae8f0847f104bd8cb16673d
jrstewart2 <jonathan.stewart2@nationwide.co.uk>
2022-12-07 15:17:57 +0000

    uploading template

---
+++ CryptoBytes-API/src/test/java/stewart/jonathan/CryptoBytes/API/CryptoBytesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package stewart.jonathan.CryptoBytes.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBytesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
